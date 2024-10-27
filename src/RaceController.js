32dea6bff0a7f5a904fe6a239581db9184a7e4f1
yoouyeon <jyeon.yoon59@gmail.com>
2024-10-28 00:42:24 +0900

    refactor(race-controller): 역할 리팩토링
    - 경주 데이터 관리 기능 RaceModel로 이동
    - run 메소드에 RaceModel의 변경된 내용 반영
    - moveCount를 tryCount로 이름 변경

--- src/RaceController.js
+++ src/RaceController.js
@@ -1,41 +1,41 @@
-import Car from "./Car.js";
 import generalValidator from "./validator/generalValidator.js";
 import carNameValidator from "./validator/carNameValidator.js";
 import moveCountValidator from "./validator/moveCountValidator.js";
-import Input from "./view/Input.js";
-import Output from "./view/Output.js";
-import RaceEngine from "./RaceEngine.js";
+import { Input, Output } from "./view/index.js";
+import Car from "./model/Car.js";
+import Race from "./model/Race.js";
 
 class RaceController {
   static #CAR_NAME_SEPARATOR = ",";
 
-  #carCollection;
-
-  #moveCount;
-
-  #raceEngine;
+  #raceModel;
 
   constructor() {
-    this.#carCollection = [];
-    this.#moveCount = 0;
-    this.#raceEngine = new RaceEngine();
+    this.#raceModel = null;
   }
 
   /**
    * 레이스를 시작하는 메서드
    * @async
+   * @returns {Promise<void>}
+   * @description
+   * 1. 자동차 이름을 입력받는다.
+   * 2. 자동차 이름을 검증하고 자동차 객체들을 생성한다.
+   * 3. 시도 횟수를 입력받는다.
+   * 4. 시도 횟수를 검증하고 숫자로 변환한다.
+   * 5. 레이스를 시작한다.
+   * 6. 레이스 결과를 확인한다.
+   * 7. 전체 레이스 결과와 최종 우승자를 출력한다.
    */
 
   async run() {
     const carNames = await Input.readCarNames();
-    this.#createCarCollection(carNames);
-    const moveCount = await Input.readMoveCount();
-    this.#setMoveCount(moveCount);
-    // TODO : race engine을 돌려 history와 winner를 반환
-    const { history, winners } = this.#raceEngine.start(
-      this.#moveCount,
-      this.#carCollection
-    );
+    const carCollection = RaceController.#createCarCollection(carNames);
+    const inputCout = await Input.readMoveCount();
+    const tryCount = RaceController.#getTryCount(inputCout);
+    this.#raceModel = new Race(carCollection, tryCount);
+    this.#raceModel.start();
+    const { history, winners } = this.#raceModel.result;
     Output.printAllRounds(history);
     Output.printWinners(winners);
   }
@@ -43,24 +43,24 @@ class RaceController {
   /**
    * @private
    * @param {string} carNames - #CAR_NAME_SEPARATOR로 구분된 자동차 이름들
+   * @returns {Array<Car>} - 자동차 객체들의 배열
    */
-  #createCarCollection(carNames) {
+  static #createCarCollection(carNames) {
     generalValidator.checkInput(carNames);
     const carNameList = carNames.split(RaceController.#CAR_NAME_SEPARATOR);
     carNameValidator.checkList(carNameList);
-    carNameList.forEach((carName) => {
-      this.#carCollection.push(new Car(carName));
-    });
+    return carNameList.map((carName) => new Car(carName));
   }
 
   /**
    * @private
    * @param {string} moveCount - 이동 횟수
+   * @returns {number} - 이동 횟수
    */
-  #setMoveCount(moveCount) {
-    generalValidator.checkInput(moveCount);
-    moveCountValidator.checkCount(moveCount);
-    this.#moveCount = Number.parseInt(moveCount, 10);
+  static #getTryCount(tryCount) {
+    generalValidator.checkInput(tryCount);
+    moveCountValidator.checkCount(tryCount);
+    return Number.parseInt(tryCount, 10);
   }
 }
 
