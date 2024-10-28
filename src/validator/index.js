ddf1d243aa041f6a6ca40fbe2a7f2fc247ed0826
yoouyeon <jyeon.yoon59@gmail.com>
2024-10-28 21:43:43 +0900

    refactor(validator): 사용하지 않는 index 파일 제거

--- src/validator/index.js
+++
@@ -1,5 +0,0 @@
-import carNameValidator from "./carNameValidator.js";
-import generalValidator from "./generalValidator.js";
-import moveCountValidator from "./moveCountValidator.js";
-
-export { carNameValidator, generalValidator, moveCountValidator };
