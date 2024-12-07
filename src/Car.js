aaf0dc503d13b538053da84e37fc515028c5a26a
yoouyeon <jyeon.yoon59@gmail.com>
2024-12-07 16:52:33 +0900

    feat(car): 자동차 전진 기능 추가
    - 자동차 전진 함수 추가
    - 테스트 코드 추가
    - 기능 목록 업데이트

--- src/Car.js
+++ src/Car.js
@@ -1,5 +1,5 @@
 // @ts-check
-
+import { MissionUtils } from '@woowacourse/mission-utils';
 import CustomError from './CustomError.js';
 
 class Car {
@@ -19,6 +19,13 @@ class Car {
     this.position = 0;
   }
 
+  get status() {
+    return {
+      name: this.name,
+      position: this.position,
+    };
+  }
+
   /**
    * @param {string} name - 자동차 이름
    * @description 자동차 이름 유효성 검사
@@ -30,6 +37,22 @@ class Car {
     if (name.length > 5) {
       throw new CustomError('이름은 5자 이하로 입력해주세요.');
     }
+  }
+
+  /**
+   * @description 자동차 전진 여부 결정 후 위치 업데이트
+   */
+  move() {
+    if (Car.pickRandomNumber() >= 4) {
+      this.position += 1;
+    }
+  }
+
+  /**
+   * @return {number} - 0부터 9까지의 랜덤 숫자
+   */
+  static pickRandomNumber() {
+    return MissionUtils.Random.pickNumberInRange(0, 9);
   }
 }
 
--- src/Car.test.js
+++ src/Car.test.js
@@ -1,4 +1,5 @@
 import Car from './Car.js';
+import mockRandoms from './mockRandoms.js';
 
 describe('자동차 테스트', () => {
   describe('이름 유효성 테스트', () => {
@@ -13,5 +14,39 @@ describe('자동차 테스트', () => {
       const validName = '12345';
       expect(() => new Car(validName)).not.toThrow();
     });
+  });
+
+  describe('자동차 전진 테스트', () => {
+    test.each([{ random: 4 }, { random: 5 }])(
+      `랜덤 값이 4 이상일 경우 전진한다. (랜덤 값: $random)`,
+      ({ random }) => {
+        // given
+        const car = new Car('pobi');
+        mockRandoms([random]);
+        const expectedPosition = 1;
+
+        // when
+        car.move();
+
+        // then
+        expect(car.status.position).toBe(expectedPosition);
+      }
+    );
+
+    test.each([{ random: 3 }, { random: 2 }])(
+      `랜덤 값이 3 이하일 경우 전진하지 않는다. (랜덤 값: $random)`,
+      ({ random }) => {
+        // given
+        const car = new Car('pobi');
+        mockRandoms([random]);
+        const expectedPosition = 0;
+
+        // when
+        car.move();
+
+        // then
+        expect(car.status.position).toBe(expectedPosition);
+      }
+    );
   });
 });
